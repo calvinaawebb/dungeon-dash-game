257dd2c54d0a4ad417295ad7292a276275f3445b
FluxChikin <calvinaawebb@gmail.com>
2023-03-05 21:47:25 -0700

    Trying to pull new version

--- Dungeon Dash/Assets/Scripts/Player_Controller.cs
+++ Dungeon Dash/Assets/Scripts/Player_Controller.cs
@@ -7,6 +7,7 @@ public class Player_Controller : MonoBehaviour
 
     public int player_num; //which player, 0 or 1? Declare in Unity Inspector
     public float speed; //how fast can character move? (could change...?)
+    public Quaternion current = new Quaternion.Euler(0,0,0);
 
     public bool isStunned = false; //becomes true to penalize player or to keep them from walking around when doing puzzles.
 
@@ -14,7 +15,6 @@ public class Player_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
 
     }
 
@@ -24,10 +24,53 @@ public class Player_Controller : MonoBehaviour
         if (!isStunned)
         {
             //player movement
+            /*
+            Vector3 pos = transform.localPosition;
+            pos.x += Input.GetAxisRaw("Horizontal_" + player_num) * speed; //change vertical position based on input from specific player
+            pos.y += Input.GetAxisRaw("Vertical_" + player_num) * speed; //change horizontal position based on input from specific player
+            transform.localPosition = pos; //update localPosition of player
+            switch(Input.GetAxisRaw("Horizontal_" + player_num)) 
+            {
+                case 1:
+                    gameObject.transform.rotation = Quaternion.Euler(0,0,90);
+                    break;
+                case -1:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
+                    break;
+            }
+            switch (Input.GetAxisRaw("Vertical_" + player_num))
+            {
+                case 1:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 180);
+                    break;
+                case -1:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+            }
+            */
             Vector3 pos = transform.localPosition;
             pos.x += Input.GetAxis("Horizontal_" + player_num) * speed; //change vertical position based on input from specific player
             pos.y += Input.GetAxis("Vertical_" + player_num) * speed; //change horizontal position based on input from specific player
             transform.localPosition = pos; //update localPosition of player
+            switch (Input.GetAxis("Horizontal_" + player_num))
+            {
+                case > 0:
+                    Quaternion
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, (Input.GetAxis("Horizontal_" + player_num)*Quaternion.Angle(Quaternion.Euler(0,0,1), (Quaternion.Angle(gameObject.transform.rotation, Quaternion.Euler(0,0,90)) >= 90f ? gameObject.transform.rotation * Quaternion.Inverse(Quaternion.Euler(0, 0, 90)) : gameObject.transform.rotation * Quaternion.Euler(0, 0, 90)))));
+                    break;
+                case -1:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
+                    break;
+            }
+            switch (Input.GetAxis("Vertical_" + player_num))
+            {
+                case 1:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 180);
+                    break;
+                case -1:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+            }
         }
 
     }
