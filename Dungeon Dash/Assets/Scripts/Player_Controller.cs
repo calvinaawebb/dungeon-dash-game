0dce520c786d347c78fb983a97b5896ac1117c7b
CentreFusion <calvinaawebb@gmail.com>
2023-03-07 20:51:40 -0700

    Scrapped better movement idea to get project along

--- Dungeon Dash/Assets/Scripts/Player_Controller.cs
+++ Dungeon Dash/Assets/Scripts/Player_Controller.cs
@@ -8,12 +8,15 @@ public class Player_Controller : MonoBehaviour
     public int player_num; //which player, 0 or 1? Declare in Unity Inspector
     public float speed; //how fast can character move? (could change...?)
     public bool isStunned = false; //becomes true to penalize player or to keep them from walking around when doing puzzles.
+    public Quaternion curAngle;
+    public Quaternion curAngle2;
+    public Quaternion angleTo;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -21,8 +24,6 @@ public class Player_Controller : MonoBehaviour
     {
         if (!isStunned)
         {
-            //player movement
-            /*
             Vector3 pos = transform.localPosition;
             pos.x += Input.GetAxisRaw("Horizontal_" + player_num) * speed; //change vertical position based on input from specific player
             pos.y += Input.GetAxisRaw("Vertical_" + player_num) * speed; //change horizontal position based on input from specific player
@@ -45,7 +46,11 @@ public class Player_Controller : MonoBehaviour
                     gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
                     break;
             }
-            */
+            /*
+            if(Input.anyKey) 
+            {
+                angleTo = gameObject.transform.rotation;
+            }
             Vector3 pos = transform.localPosition;
             pos.x += Input.GetAxis("Horizontal_" + player_num) * speed; //change vertical position based on input from specific player
             pos.y += Input.GetAxis("Vertical_" + player_num) * speed; //change horizontal position based on input from specific player
@@ -53,7 +58,26 @@ public class Player_Controller : MonoBehaviour
             switch (Input.GetAxis("Horizontal_" + player_num))
             {
                 case > 0:
-                    gameObject.transform.rotation = Quaternion.Euler(0, 0, (Input.GetAxis("Horizontal_" + player_num)*Quaternion.Angle(Quaternion.Euler(0,0,1), (Quaternion.Angle(gameObject.transform.rotation, Quaternion.Euler(0,0,90)) >= 90f ? gameObject.transform.rotation * Quaternion.Inverse(Quaternion.Euler(0, 0, 90)) : gameObject.transform.rotation * Quaternion.Euler(0, 0, 90)))));
+                    curAngle = new Quaternion(0,0,90,0);
+                    curAngle2 = new Quaternion(0,0,270,0);
+                    if(angleTo.z < 0) 
+                    {
+                        angleTo = angleTo * Quaternion.Euler(0,0,360);
+                    }
+                    if (angleTo.z >= curAngle.z && angleTo.z <= curAngle2.z) 
+                    {
+                        if (gameObject.transform.rotation != curAngle) 
+                        {
+                            gameObject.transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal_" + player_num)*Quaternion.Inverse(curAngle).z);
+                        }
+                    } else if (angleTo.z < curAngle.z) 
+                    {
+                        if (gameObject.transform.rotation != curAngle) 
+                        {
+                            gameObject.transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal_" + player_num)*curAngle.z);
+                        }
+                    }
+                    //gameObject.transform.rotation = Quaternion.Euler(0, 0, (Input.GetAxis("Horizontal_" + player_num)*Quaternion.Angle(Quaternion.Euler(0,0,1), (Quaternion.Angle(gameObject.transform.rotation, Quaternion.Euler(0,0,90)) >= 90f ? gameObject.transform.rotation * Quaternion.Inverse(Quaternion.Euler(0, 0, 90)) : gameObject.transform.rotation * Quaternion.Euler(0, 0, 90)))));
                     break;
                 case -1:
                     gameObject.transform.rotation = Quaternion.Euler(0, 0, -90);
@@ -68,6 +92,7 @@ public class Player_Controller : MonoBehaviour
                     gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
                     break;
             }
+            */
         }
 
     }
